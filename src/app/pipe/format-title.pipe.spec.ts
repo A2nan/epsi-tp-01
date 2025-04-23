12d1bb44658e2660528234b2afd4aecb107e5373
Mahboubi Adnan <adnan.mahboubi03@gmail.com>
2025-04-23 21:38:33 +0200

    TODO 2 correction du pipe

--- src/app/pipe/format-title.pipe.spec.ts
+++ src/app/pipe/format-title.pipe.spec.ts
@@ -1,18 +1,8 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { FormatTitlePipe } from './format-title.pipe';
 
-@Pipe({
-  name: 'formatTitle',
-  standalone: true
-})
-export class FormatTitlePipe implements PipeTransform {
-  transform(value: string | null | undefined): string {
-    console.log(value);
-
-    if (!value) return ''; // Gère les cas où value est null, undefined ou vide
-    const words = value.replace(/_/g, ' ').toLowerCase().split(' ');
-    
-    return words.map(word => 
-      word.charAt(0).toUpperCase() + word.slice(1)
-    ).join(' ');
-  }
-}
+describe('FormatTitlePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatTitlePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/pipe/format-title.pipe.ts
+++ src/app/pipe/format-title.pipe.ts
@@ -5,8 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatTitlePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string | null | undefined): string {
+    if (!value) return '';
+
+    const words = value.replace(/_/g, ' ').toLowerCase().split(' ');
+
+    return words.map(word =>
+      word.charAt(0).toUpperCase() + word.slice(1)
+    ).join(' ');
   }
 
 }
